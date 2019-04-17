8a06547b8c8618ff4f39a500a61e34c009b7f987
Tony <tony.yang98@gmail.com>
2019-04-17 10:43:30 -0400

    include new welcome activity

--- app/src/main/java/com/example/tattlerapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/tattlerapplication/WelcomeActivity.java
@@ -2,12 +2,16 @@ package com.example.tattlerapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        logo = findViewById(R.id.logo);
     }
 }
